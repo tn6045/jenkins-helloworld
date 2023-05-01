fef83da45535cf20cca32624d492e6b9b19d3aa9
tn6045 <123999480+tn6045@users.noreply.github.com>
2023-05-01 19:59:49 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,32 +1,10 @@
-/*public class main
+public class main
 {
-public static void main (String[] args)
-{
-System.out.println ("hello world");
+        public static void main (String[] args)
+        {
+                System.out.println ("hello world");
+                Date date = new Date();
+                System.out.println(date);
+        }
 }
-}*/
 
-
-import java.util.Scanner;
-        public class CalculatePI {
-
-            public static void main(String[] args) {
-                // Create a Scanner object
-                Scanner input = new Scanner (System.in);
-
-                // Prompt the user to enter input
-                System.out.println("Enter number of terms");
-                double i = input.nextDouble(); // value of i user entered
-                    double sum = 0;
-                for(i=0; i<10000; i++){
-                           if(i%2 == 0) // if the remainder of `i/2` is 0
-                           sum += -1 / ( 2 * i - 1);
-                        else
-                           sum += 1 / (2 * i - 1);
-    }
-
-                        System.out.println(sum);
-
-
-    }
-}
